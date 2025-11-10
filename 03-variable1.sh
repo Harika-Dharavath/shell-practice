2b6f73a5392c1f34bbf8caf14f3f5cc12396c246
harika dharavath <harika@joindevops.com>
2025-11-10 22:21:45 +0530

    message

---
+++ 03-variable1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ echo "please enter your PIN NUMBER" # prompt the user to enter their PIN number
+ read PIN_NUMBER # read the user input and store it in the variable PIN_NUMBER
+ echo "your PIN NUMBER is $PIN_NUMBER" # This prints the value the user entered
