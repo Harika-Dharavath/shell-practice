302d2a73605a3634293843804bc3fc6c1e386404
harika dharavath <harika@joindevops.com>
2025-11-11 13:57:08 +0530

    some message

---
+++ 07.special-var.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "all positional parameters: $*"
+echo "number of positional parameters: $#"
+echo "script name: $0"
+echo "first positional parameter: $1"
+echo " all positional parameters as array: $@"
