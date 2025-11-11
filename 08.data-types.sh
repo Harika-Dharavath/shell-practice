bd0b07273e31cdbb2cdcc23ffa0f1453c3c17bb6
harika dharavath <harika@joindevops.com>
2025-11-11 18:19:33 +0530

    some message

--- 08.data-types.sh
+++ 08.data-types.sh
@@ -2,6 +2,7 @@
 #everyhting in shell is considered as string
 NUMBER1=100
 NUMBER2=200
+NAME=HARIKA 
 
-SUM=$((NUMBER1 + NUMBER2))
+SUM=$((NUMBER1 + NUMBER2+NAME))
 echo "Sum of $NUMBER1 and $NUMBER2 is: $SUM"
