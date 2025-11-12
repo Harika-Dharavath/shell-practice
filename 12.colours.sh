b7fbc6e58e470f564be73a77f1be8d588da1278a
harika dharavath <harika@joindevops.com>
2025-11-12 17:26:49 +0530

    message

--- 12.colours.sh
+++ 12.colours.sh
@@ -6,7 +6,8 @@ Y="\e[0;33m" # Yellow color]
 B="\e[1;33M" # Bold Yellow color
 O="\e[1;34m" # Bold Blue color
 N="\e[0m"  # No Color
-echo -e "$R i love red colour " 
+echo -e "$R i love red colour $N" 
 #RED COLOUR next line ki shift avvakunda undadaniki $N is added
-echo -e "$G my mother  love green colour $N"
+echo -e "$G my mother  love green colour "
+#even if u dont add $N it will work only for next line
 echo -e "$Y my father love yellow colour $N"
---
+++ 13.opti-colours.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+R="\e[31m" # Red color
+G="\e[32m" # Green color
+Y="\e[0;33m" # Yellow color]
+B="\e[1;33M" # Bold Yellow color
+O="\e[1;34m" # Bold Blue color
+N="\e[0m"  # No Color
+
+
+USERID=$(id -u) # prints the user id of current user
+if [ $USERID -ne 0 ]; then
+   echo  " $R You must run this script as root user."  
+   exit 1
+fi
+
+VALIDATECOMMAND(){ #no space should be between validate command and ()
+    if [ $1 -ne 0 ]; then
+        echo "$B Error: $2 installation failed."
+        #ACCORDING to our present code $2 is mysql ,mgodb,ngnix
+        exit 1
+        
+
+    else 
+        echo "$O $2 installed successfully."
+    fi
+}             
+
+dnf install nginx -y
+VALIDATECOMMAND $? "Nginx" #without calling it will not execute here we called the validate function
+
+
+dnf install mysql -y
+VALIDATECOMMAND $? "Mysql"
+#ealier i gave mysql at last as mangobd couldnt install it have stopped there only mysql is also not installed so now i changed mysql in middle it have executed
+
+dnf install python3 -y
+VALIDATECOMMAND $? "python3"
+
+dnf install mongodb-mongosh -y
+VALIDATECOMMAND $? "Mongodb" #without calling it will not execute
+#mangodb is not excuted because we didnt add mangodb repository
+
+
+ 
