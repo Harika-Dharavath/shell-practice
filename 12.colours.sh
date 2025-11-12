13b2c6ab4eef58a8a0d8cbd6b2be57273aa82984
harika dharavath <harika@joindevops.com>
2025-11-12 17:19:00 +0530

    message

---
+++ 12.colours.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+R="\e[31m" # Red color
+G="\e[32m" # Green color
+Y="\e[0;33m" # Yellow color]
+B="\e[1;33M" # Bold Yellow color
+O="\e[1;34m" # Bold Blue color
+N="\e[0m"  # No Color
+echo -e "$R i love red colour $N" 
+#RED COLOUR next line ki shift avvakunda undadaniki $N is added
+echo -e "$G my mother  love green colour $N"
+echo -e "$Y my father love yellow colour $N"
