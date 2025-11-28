fc6ec98c2203df5d2b4b5c4df233bd758360e7f8
harika dharavath <harika@joindevops.com>
2025-11-28 21:14:40 +0530

    message

--- cpu-usage.sh
+++ cpu-usage.sh
@@ -3,9 +3,17 @@
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=2 #normally in projects we use 75
 
-while IFS=',' read -r line  #IFS internal field seperator
-#IFS= ',' read -r line #its separated by ,”.  
+while IFS= read -r line  #IFS internal field seperator
+                         #IFS= ',' read name age <<< harika,27  then output lo coma deggara slipt aitadi #its separated by ,”.  
 do
-    echo "$line"
+
+  USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+  PARTITION=$(echo $line | awk '{print $7}')  
+
+   if [ "$USAGE" -ge "$DISK_THRESHOLD" ]; then
+     echo "Alert: Disk usage is at ${USAGE}% on the following partition:"
+     echo "$line"
+   fi
+      
 done <<< "$DISK_USAGE" # "<<<" string	Read input from a string
-#👉 Take the content stored in $DISK_USAGE
+                           #👉 Take the content stored in $DISK_USAGE
